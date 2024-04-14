eaa47a73d8c3732b32de259dd387e20744c8adca
Ruknuddin asrari <ruknuddin.asrari@gmail.com>
2024-04-14 11:42:18 -0700

    final

---
+++ build/CMakeFiles/tictactoe.dir/C_/pico-sdk/src/rp2_common/pico_float/float_math.c.obj.d
@@ -0,0 +1,32 @@
+CMakeFiles/tictactoe.dir/C_/pico-sdk/src/rp2_common/pico_float/float_math.c.obj: \
+ C:\pico-sdk\src\rp2_common\pico_float\float_math.c \
+ C:\pico-sdk\src\common\pico_base\include/pico/types.h \
+ C:\pico-sdk\src\common\pico_base\include/pico/assert.h \
+ c:\arm-gcc\10.2021.10\lib\gcc\arm-none-eabi\10.3.1\include\stdbool.h \
+ c:\arm-gcc\10.2021.10\arm-none-eabi\include\assert.h \
+ c:\arm-gcc\10.2021.10\arm-none-eabi\include\_ansi.h \
+ c:\arm-gcc\10.2021.10\arm-none-eabi\include\newlib.h \
+ c:\arm-gcc\10.2021.10\arm-none-eabi\include\_newlib_version.h \
+ c:\arm-gcc\10.2021.10\arm-none-eabi\include\sys\config.h \
+ c:\arm-gcc\10.2021.10\arm-none-eabi\include\machine\ieeefp.h \
+ c:\arm-gcc\10.2021.10\arm-none-eabi\include\sys\features.h \
+ c:\arm-gcc\10.2021.10\lib\gcc\arm-none-eabi\10.3.1\include\stdint.h \
+ c:\arm-gcc\10.2021.10\arm-none-eabi\include\stdint.h \
+ c:\arm-gcc\10.2021.10\arm-none-eabi\include\machine\_default_types.h \
+ c:\arm-gcc\10.2021.10\arm-none-eabi\include\sys\_intsup.h \
+ c:\arm-gcc\10.2021.10\arm-none-eabi\include\sys\_stdint.h \
+ c:\arm-gcc\10.2021.10\lib\gcc\arm-none-eabi\10.3.1\include\stddef.h \
+ C:\pico-sdk\src\rp2_common\pico_float\include/pico/float.h \
+ c:\arm-gcc\10.2021.10\arm-none-eabi\include\math.h \
+ c:\arm-gcc\10.2021.10\arm-none-eabi\include\sys\reent.h \
+ c:\arm-gcc\10.2021.10\arm-none-eabi\include\_ansi.h \
+ c:\arm-gcc\10.2021.10\arm-none-eabi\include\sys\_types.h \
+ c:\arm-gcc\10.2021.10\arm-none-eabi\include\machine\_types.h \
+ c:\arm-gcc\10.2021.10\arm-none-eabi\include\sys\lock.h \
+ c:\arm-gcc\10.2021.10\arm-none-eabi\include\sys\cdefs.h \
+ c:\arm-gcc\10.2021.10\lib\gcc\arm-none-eabi\10.3.1\include\float.h \
+ C:\pico-sdk\src\rp2_common\pico_bootrom\include/pico/bootrom/sf_table.h \
+ C:\pico-sdk\src\rp2_common\pico_platform\include/pico/platform.h \
+ C:\pico-sdk\src\rp2040\hardware_regs\include/hardware/platform_defs.h \
+ C:\pico-sdk\src\rp2040\hardware_regs\include/hardware/regs/addressmap.h \
+ C:\pico-sdk\src\rp2040\hardware_regs\include/hardware/regs/sio.h
---
+++ build/CMakeFiles/tictactoe.dir/C_/pico-sdk/src/rp2_common/pico_standard_link/new_delete.cpp.obj.d
@@ -0,0 +1,25 @@
+CMakeFiles/tictactoe.dir/C_/pico-sdk/src/rp2_common/pico_standard_link/new_delete.cpp.obj: \
+ C:\pico-sdk\src\rp2_common\pico_standard_link\new_delete.cpp \
+ c:\arm-gcc\10.2021.10\arm-none-eabi\include\c++\10.3.1\cstdlib \
+ c:\arm-gcc\10.2021.10\arm-none-eabi\include\c++\10.3.1\arm-none-eabi\thumb\v6-m\nofp\bits\c++config.h \
+ c:\arm-gcc\10.2021.10\arm-none-eabi\include\c++\10.3.1\arm-none-eabi\thumb\v6-m\nofp\bits\os_defines.h \
+ c:\arm-gcc\10.2021.10\arm-none-eabi\include\c++\10.3.1\arm-none-eabi\thumb\v6-m\nofp\bits\cpu_defines.h \
+ c:\arm-gcc\10.2021.10\arm-none-eabi\include\c++\10.3.1\pstl\pstl_config.h \
+ c:\arm-gcc\10.2021.10\arm-none-eabi\include\stdlib.h \
+ c:\arm-gcc\10.2021.10\arm-none-eabi\include\machine\ieeefp.h \
+ c:\arm-gcc\10.2021.10\arm-none-eabi\include\_ansi.h \
+ c:\arm-gcc\10.2021.10\arm-none-eabi\include\newlib.h \
+ c:\arm-gcc\10.2021.10\arm-none-eabi\include\_newlib_version.h \
+ c:\arm-gcc\10.2021.10\arm-none-eabi\include\sys\config.h \
+ c:\arm-gcc\10.2021.10\arm-none-eabi\include\sys\features.h \
+ c:\arm-gcc\10.2021.10\lib\gcc\arm-none-eabi\10.3.1\include\stddef.h \
+ c:\arm-gcc\10.2021.10\arm-none-eabi\include\sys\reent.h \
+ c:\arm-gcc\10.2021.10\arm-none-eabi\include\_ansi.h \
+ c:\arm-gcc\10.2021.10\arm-none-eabi\include\sys\_types.h \
+ c:\arm-gcc\10.2021.10\arm-none-eabi\include\machine\_types.h \
+ c:\arm-gcc\10.2021.10\arm-none-eabi\include\machine\_default_types.h \
+ c:\arm-gcc\10.2021.10\arm-none-eabi\include\sys\lock.h \
+ c:\arm-gcc\10.2021.10\arm-none-eabi\include\sys\cdefs.h \
+ c:\arm-gcc\10.2021.10\arm-none-eabi\include\machine\stdlib.h \
+ c:\arm-gcc\10.2021.10\arm-none-eabi\include\alloca.h \
+ c:\arm-gcc\10.2021.10\arm-none-eabi\include\c++\10.3.1\bits\std_abs.h
